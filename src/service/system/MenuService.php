87baa1e5da7c4b95c6b092dd94732b7f68a277e5
sunboy <qysk@vip.qq.com>
2025-01-10 22:10:40 +0800

    更正细节

--- src/service/system/MenuService.php
+++ src/service/system/MenuService.php
@@ -15,17 +15,30 @@ class MenuService
      * @return void
      * @throws Exception
      */
-    public function insertMenus(array $menus, ?string $parentSlot = null): void
+    public function insertMenusBySlot(array $menus, ?string $parentSlot = null): void
     {
         if (!empty($parentSlot)) {
             $parentSlotid = SystemMenu::where('slot', $parentSlot)->value('id');
         } else {
             $parentSlotid = 0;
         }
+        $this->insertMenus($menus, $parentSlotid);
+    }
+
+    /**
+     * 插入菜单数据。
+     *
+     * @param array $menus 菜单数据数组。
+     * @param string|null $parentSlot 父级 slot 标记。
+     * @return void
+     * @throws Exception
+     */
+    public function insertMenus(array $menus, ?string $pid = null): void
+    {
 
         foreach ($menus as $menu) {
             // 处理顶级菜单
-            $parentId = $this->insertMenu($menu, $parentSlotid);
+            $parentId = $this->insertMenu($menu, $pid);
             if (!empty($menu['sub'])) {
                 // 如果有子菜单，则递归插入子菜单，并设置父级ID
                 $this->insertMenus($menu['sub'], $parentId);
