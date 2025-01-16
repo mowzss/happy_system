108729fe7245acfaa0e36086ba6910d7d088ea79
sunboy <qysk@vip.qq.com>
2025-01-17 01:00:04 +0800

    init

--- src/home/index/Index.php
+++ src/home/index/Index.php
@@ -5,6 +5,7 @@ declare (strict_types=1);
 namespace app\home\index;
 
 use app\common\controllers\BaseHome;
+use mowzs\lib\module\service\ContentBaseService;
 
 class Index extends BaseHome
 {
@@ -16,5 +17,40 @@ class Index extends BaseHome
 
 
         return $this->fetch();
+    }
+
+    public function text()
+    {
+        $array = [
+            "pagenum" => NULL,
+            "rows" => 10,
+            "paginate" => 0,
+            "where" => [
+                [
+                    "view",
+                    "<",
+                    "800"
+                ],
+                [
+                    "is_pic",
+                    "=",
+                    "1"
+                ],
+                [
+                    "status",
+                    "=",
+                    1
+                ],
+                [
+                    "cid",
+                    "in",
+                    [11]
+                ]
+            ],
+            "by" => "desc",
+            "order" => "view",
+        ];
+        ContentBaseService::instance(['article'])->getList($array);
+        echo '';
     }
 }
