3e79668459fda58b8b0819c13506b23887a6ca99
sunboy <qysk@vip.qq.com>
2025-03-09 11:48:21 +0800

    feat(style): 新增万年历组件样式
    - 添加了万年历组件的样式文件，包括移动端和PC端版本
    - 样式文件位于 happy-style-xz项目的 static 目录下
    - 移动端样式文件路径：style/happy-style-xz/src/static/xz_style/xingzuo_h5/css/module/_calendar.less
    - PC端样式文件路径：style/happy-style-xz/src/static/xz_style/xingzuo_pc/css/module/_calendar.less

--- src/admin/system/Sitemap.php
+++ src/admin/system/Sitemap.php
@@ -3,8 +3,127 @@
 namespace app\admin\system;
 
 use app\common\controllers\BaseAdmin;
+use app\common\traits\CrudTrait;
+use app\model\system\SystemModule;
+use app\model\system\SystemSitemap;
+use think\App;
 
 class Sitemap extends BaseAdmin
 {
+    use CrudTrait;
+
+    /**
+     * @var string
+     */
+    protected string $title = 'Sitemap网站地图';
+    /**
+     * 默认排序
+     * @var array
+     */
+    protected array $default_order = [
+        'create_time' => 'desc'
+    ];
+    /**
+     * @var array|string[]
+     */
+    protected array $class_name = [
+        'content' => '内容',
+        'column' => '栏目',
+        'tag' => '标签'
+    ];
+    /**
+     * 模块列表
+     * @var array
+     */
+    protected array $modules;
+
+    public function __construct(SystemSitemap $model, App $app)
+    {
+        parent::__construct($app);
+        $this->model = $model;
+        $this->modules = (new SystemModule())->column('title', 'dir');
+        $this->setParams();
+
+    }
+
+    /**
+     * @return void
+     */
+    protected function setParams(): void
+    {
+        $this->tables = [
+            'fields' => [
+                [
+                    'field' => 'id',
+                    'title' => 'ID',
+                    'width' => 80,
+                    'sort' => true,
+                ],
+                [
+                    'field' => 'module',
+                    'title' => '模块',
+                    'width' => 160,
+                    'align' => 'content'
+                ], [
+                    'field' => 'class',
+                    'title' => '数据源',
+                    'width' => 120,
+                ], [
+                    'field' => 'url',
+                    'title' => '链接地址',
+                    'align' => 'left',
+                ], [
+                    'field' => 'type',
+                    'title' => '格式',
+                    'width' => 80,
+
+                ]
+            ],
+            'top_button' => [
+
+            ],
+            'right_button' => [
+
+            ]
+
+        ];
+
+        $this->forms = [
+            'fields' => [
+                [
+                    'type' => 'select',
+                    'name' => 'module',
+                    'label' => '模块',
+                    'options' => $this->modules,
+                    'required' => true
+                ], [
+                    'type' => 'select',
+                    'name' => 'class',
+                    'label' => '数据',
+                    'options' => $this->class_name,
+                    'required' => true,
+                ]
+            ]
+        ];
+        $this->search = [
+            'id#=#id', 'module#like#name', 'class#=#class', 'create_time#between#create_time'
+        ];
+    }
+
+    /**
+     * 处理列表数据
+     * @param $data
+     * @return void
+     */
+    protected function _index_list_filter(&$data): void
+    {
+
+        foreach ($data['data'] as &$vo) {
+            $vo['class'] = $this->class_name[$vo['class']];
+            $vo['module'] = $this->modules[$vo['module']];
+            $vo['url'] = $this->request->domain() . $vo['url'];
+        }
+    }
+
 
 }
