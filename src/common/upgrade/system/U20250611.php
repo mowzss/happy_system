62c6ea42209e956def60e736683d5248e84e2c4b
sunboy <qysk@vip.qq.com>
2025-06-11 17:50:04 +0800

    feat(system): 添加默认富文本编辑器配置项
    - 在系统配置中新增 editor_default 配置项，用于设置默认富文本编辑器
    - 修改 textarea 元素的 data-type 属性，使用系统配置的默认编辑器
    - 可选的富文本编辑器包括 tinymce、wangeditor 和 ueditor

---
+++ src/common/upgrade/system/U20250611.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\common\upgrade\system;
+
+use app\model\system\SystemConfig;
+use app\model\system\SystemConfigGroup;
+
+class U20250611
+{
+    public function run()
+    {
+        $this->upConfig();
+    }
+
+    private function upConfig()
+    {
+        $group_id = SystemConfigGroup::where(['title' => '基础设置', 'module' => 'system'])->value('id');
+        if (!empty($group_id)) {
+            if (empty(SystemConfig::where(['group_id' => $group_id, 'name' => 'editor_default'])->value('id'))) {
+                SystemConfig::create([
+                    'name' => 'editor_default',
+                    'type' => 'text',
+                    'title' => '默认富文本编辑器',
+                    'group_id' => $group_id,
+                    'options' => 'tinymce|tinymce编辑器' . PHP_EOL . 'wangeditor|WangEditor' . PHP_EOL . 'ueditor|百度Ueditor',
+                    'help' => '模型设计中默认的富文本编辑器，如模型设计中指定了编辑器，则此设置无效',
+                    'value' => '',
+                    'extend' => NULL,
+                    'list' => '0',
+                    'module' => 'system',
+                    'status' => '1',
+                ]);
+            }
+        }
+
+    }
+}
