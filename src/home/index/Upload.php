a881438e739c0e1f18a4965e7391439984d09980
sunboy <qysk@vip.qq.com>
2026-03-02 17:33:07 +0800

    feat(upload): 添加用户信息获取和系统配置检查功能
    - 引入 ConfigLogic 和 UserHelper 类用于系统配置和用户信息处理
    - 在初始化方法中设置视图并获取当前用户信息
    - 添加系统安装状态检查，未安装时重定向到安装页面
    - 集成系统配置逻辑以获取网站配置信息

--- src/home/index/Upload.php
+++ src/home/index/Upload.php
@@ -5,6 +5,8 @@ namespace app\home\index;
 
 use app\common\controllers\BaseHome;
 use app\common\traits\UploadTraits;
+use app\logic\system\ConfigLogic;
+use mowzs\lib\helper\UserHelper;
 
 class Upload extends BaseHome
 {
@@ -12,5 +14,12 @@ class Upload extends BaseHome
 
     protected function initialize(): void
     {
+        $this->setView();
+        $this->user = UserHelper::instance()->getUserInfo();
+        if ($this->app->config->get('happy.installed', false)) {
+            $this->web_config = ConfigLogic::instance()->getConfigValue();
+        } else {
+            $this->redirect(urls('install/index/index'));
+        }
     }
 }
