b124657f335f2cb0b19130dbf778e27073fb9878
sunboy <qysk@vip.qq.com>
2026-03-02 17:34:44 +0800

    refactor(home): 移除上传控制器中的初始化逻辑
    - 删除了 ConfigLogic 和 UserHelper 的引入
    - 移除了 initialize 方法中的视图设置和用户信息获取
    - 移除了系统配置加载和安装状态检查逻辑
    - 简化了 Upload 控制器的依赖关系
    - 保留了 UploadTraits 特性的使用

--- src/home/index/Upload.php
+++ src/home/index/Upload.php
@@ -5,21 +5,8 @@ namespace app\home\index;
 
 use app\common\controllers\BaseHome;
 use app\common\traits\UploadTraits;
-use app\logic\system\ConfigLogic;
-use mowzs\lib\helper\UserHelper;
 
 class Upload extends BaseHome
 {
     use UploadTraits;
-
-    protected function initialize(): void
-    {
-        $this->setView();
-        $this->user = UserHelper::instance()->getUserInfo();
-        if ($this->app->config->get('happy.installed', false)) {
-            $this->web_config = ConfigLogic::instance()->getConfigValue();
-        } else {
-            $this->redirect(urls('install/index/index'));
-        }
-    }
 }
