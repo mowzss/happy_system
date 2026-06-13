05e21e678910b8fc36f0f7239e88a5942f19dd17
sunboy <qysk@vip.qq.com>
2026-06-13 16:59:31 +0800

    refactor(system): 更新系统配置模块代码
    - 为 updateModuleConfig 方法添加返回类型声明 void
    - 添加方法文档注释说明返回值类型
    - 将变量名从 $system_config_group 统一改为 $group_model
    - 移除不必要的空检查逻辑，直接执行数据库操作
    - 优化代码结构提高可读性

--- src/common/upgrade/system/U2026061301.php
+++ src/common/upgrade/system/U2026061301.php
@@ -12,57 +12,60 @@ class U2026061301
         $this->updateModuleConfig();
     }
     
-    private function updateModuleConfig()
+    /**
+     * @return void
+     */
+    private function updateModuleConfig(): void
     {
-        $system_config_group = new SystemConfigGroup();
+        $group_model = new SystemConfigGroup();
         $config_group_data = [
             'title' => '手机版设置',
             'sys_show' => 1,
             'module' => 'system',
             'status' => 1,
         ];
-        $config_group_id = $system_config_group->save($config_group_data);
-        if (!empty($config_group_id->id)) {
-            SystemConfig::saveAll([
-                [
-                    'name' => 'site_wap_domain',
-                    'type' => 'text',
-                    'title' => '手机版域名',
-                    'group_id' => $config_group_id->id,
-                    'options' => '',
-                    'help' => '填写域名，需要包含http://或https:// 无需/结尾',
-                    'value' => '',
-                    'extend' => NULL,
-                    'list' => '0',
-                    'module' => 'system',
-                    'status' => '1',
-                ], [
-                    'name' => 'is_wap_domain',
-                    'type' => 'radio',
-                    'title' => '是否使用手机版域名',
-                    'group_id' => $config_group_id->id,
-                    'options' => '0|否' . PHP_EOL . '1|是',
-                    'help' => '是否使用手机版独立域名访问',
-                    'value' => '0',
-                    'extend' => NULL,
-                    'list' => '0',
-                    'module' => 'system',
-                    'status' => '1',
-                ], [
-                    'name' => 'is_wap_domain_dump',
-                    'type' => 'radio',
-                    'title' => '手机版域名是否重定向',
-                    'group_id' => $config_group_id->id,
-                    'options' => '0|否' . PHP_EOL . '1|是',
-                    'help' => '开启重定向后，pc端访问手机版域名时，将自动重定向到pc端域名',
-                    'value' => '0',
-                    'extend' => NULL,
-                    'list' => '0',
-                    'module' => 'system',
-                    'status' => '1',
-                ],
-            ]);
-        }
+        $group_model = $group_model->save($config_group_data);
+        
+        SystemConfig::saveAll([
+            [
+                'name' => 'site_wap_domain',
+                'type' => 'text',
+                'title' => '手机版域名',
+                'group_id' => $group_model->id,
+                'options' => '',
+                'help' => '填写域名，需要包含http://或https:// 无需/结尾',
+                'value' => '',
+                'extend' => NULL,
+                'list' => '0',
+                'module' => 'system',
+                'status' => '1',
+            ], [
+                'name' => 'is_wap_domain',
+                'type' => 'radio',
+                'title' => '是否使用手机版域名',
+                'group_id' => $group_model->id,
+                'options' => '0|否' . PHP_EOL . '1|是',
+                'help' => '是否使用手机版独立域名访问',
+                'value' => '0',
+                'extend' => NULL,
+                'list' => '0',
+                'module' => 'system',
+                'status' => '1',
+            ], [
+                'name' => 'is_wap_domain_dump',
+                'type' => 'radio',
+                'title' => '手机版域名是否重定向',
+                'group_id' => $group_model->id,
+                'options' => '0|否' . PHP_EOL . '1|是',
+                'help' => '开启重定向后，pc端访问手机版域名时，将自动重定向到pc端域名',
+                'value' => '0',
+                'extend' => NULL,
+                'list' => '0',
+                'module' => 'system',
+                'status' => '1',
+            ],
+        ]);
     }
+    
     
 }
