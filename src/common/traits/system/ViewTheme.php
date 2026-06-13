ca16b389c065f692bab8ab123b779b9a1efec8ab
sunboy <qysk@vip.qq.com>
2026-06-13 11:49:48 +0800

    refactor(ViewTheme): 重构视图主题逻辑并优化代码结构
    - 调整了类顶部的引入顺序，将 Request 和 Env 引入移到前面
    - 简化了 getStylePath 方法中的条件判断逻辑
    - 添加了 isDomainMobile 方法来处理移动端域名检测
    - 使用 === 替换了 == 进行严格比较
    - 优化了代码格式和空行布局，提高了可读性
    - 在 getTheme 方法中使用新的 isDomainMobile 方法进行移动端检测

--- src/common/traits/system/ViewTheme.php
+++ src/common/traits/system/ViewTheme.php
@@ -3,11 +3,11 @@ declare(strict_types=1);
 
 namespace app\common\traits\system;
 
-use think\db\exception\DataNotFoundException;
-use think\db\exception\DbException;
-use think\db\exception\ModelNotFoundException;
 use think\facade\Env;
 use think\facade\Request;
+use think\db\exception\DbException;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\ModelNotFoundException;
 
 trait ViewTheme
 {
@@ -22,7 +22,7 @@ trait ViewTheme
     {
         $this->app->config->set(['view_dir_name' => $this->getPath()], 'view');
     }
-
+    
     /**
      * 获取模板根路径
      * @return string
@@ -38,12 +38,12 @@ trait ViewTheme
             // 根据控制器层和设备类型获取模板风格
             $theme = $this->getTheme();
         }
-
-
+        
+        
         // 构建完整的模板路径
         return 'view' . DIRECTORY_SEPARATOR . $this->getStylePath() . DIRECTORY_SEPARATOR . $theme;
     }
-
+    
     /**
      * 获取模板风格目录
      * @return string
@@ -54,9 +54,9 @@ trait ViewTheme
     protected function getTheme(): string
     {
         // 获取当前请求的设备类型
-        $isMobile = Request::instance()->isMobile();
+        $isMobile = $this->isDomainMobile();
         $controllerLayer = Env::get('CONTROLLER_LAYER');
-
+        
         // 使用与 getStylePath 相同的逻辑判断控制器层
         if ($controllerLayer === 'admin') {
             $configKey = 'admin_style';
@@ -65,23 +65,23 @@ trait ViewTheme
         } else {
             $configKey = $isMobile ? 'home_wap_style' : 'home_pc_style';
         }
-
+        
         try {// 从配置中读取模板风格，如果没有设置则使用默认值
             $theme = sys_config($configKey, $this->getDefaultTheme($configKey));
         } catch (DataNotFoundException|DbException $e) {
             $theme = $this->getDefaultTheme($configKey);
         }
-
+        
         // 如果是 WAP 风格，设置缓存前缀
         if (in_array($configKey, ['user_wap_style', 'home_wap_style'])) {
             $this->app->config->set(['cache_prefix' => 'wap_'], 'view');
         } else {
             $this->app->config->set(['cache_prefix' => 'pc_'], 'view');
         }
-
+        
         return $theme;
     }
-
+    
     /**
      * 获取默认的模板风格
      * @param string $configKey
@@ -98,7 +98,26 @@ trait ViewTheme
             default => 'default'
         };
     }
-
+    
+    /**
+     * @return true
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    protected function isDomainMobile(): bool
+    {
+        $isMobile = Request::instance()->isMobile();
+        try {
+            if (!empty((int)sys_config('is_wap_domain', 0)) && format_url(sys_config('site_wap_domain'), 'host') === Request::instance()->host()) {
+                $isMobile = true;
+            }
+        } catch (DataNotFoundException|ModelNotFoundException|DbException|\Throwable $e) {
+            $isMobile = false;
+        }
+        return $isMobile;
+    }
+    
     /**
      * 获取模板风格路径
      * @return string
@@ -106,14 +125,12 @@ trait ViewTheme
     protected function getStylePath(): string
     {
         // 使用现有逻辑判断控制器层
-        if (Env::get('CONTROLLER_LAYER') == 'admin') {
+        if (Env::get('CONTROLLER_LAYER') === 'admin') {
             return 'admin_style';
-        } else {
-            if ($this->request->layer(true) == 'user') {
-                return 'user_style';
-            } else {
-                return 'home_style';
-            }
         }
+        if ($this->request->layer(true) === 'user') {
+            return 'user_style';
+        }
+        return 'home_style';
     }
 }
